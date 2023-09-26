50182e28f729446a20aa80b6eb4529ed2506e313
sean-madu <madusean2@gmail.com>
2023-09-26 10:47:23 -0600

    Add 405's to requests we can't handle

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +28,26 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        print("Got a request of: %s\n" % self.data)
+        response = 'HTTP/1.0 200 OK\n\n'
+        content = ''
+        # decode byte array into string so we can do string operations on it
+        string_data = self.data.decode('utf-8')
+
+        # get all the headers in easier to get format
+        headers = string_data.split("\n")
+        # if not a GET request return 405
+        if (not headers[0].startswith("GET")):
+            response = 'HTTP/1.1 405 Method Not Allowed\nContent-Type: text/html\nAllow: GET'
+            content = "<h1> 405 :( This assingment said to only support GET requests"
+        else:
+            pass
+
+        self.request.sendall(bytearray(response + content, 'utf-8'))
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
